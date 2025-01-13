e7d49b507d972a3cb4b4eeb6f152db66ba59e5a7
pradip2022 <Pradipbudathoki2018@gmail.com>
2025-01-13 14:30:29 -0600

    WIP

---
+++ src/Questions.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Questions {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public void question() throws IOException {
+        //reader may need to be added in the function
+        String lol = reader.readLine();
+
+    }
+}

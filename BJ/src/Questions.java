9ef9b4b2b122987cbe43e479cbd94cf915dcdcf5
pradipbudathoki <Pradipbudathoki2018@gmail.com>
2025-01-24 12:40:33 -0600

    Question Class functions as expected

--- BJ/src/Questions.java
+++ BJ/src/Questions.java
@@ -3,10 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Questions {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static boolean accepted = false;
     public void question() throws IOException {
-        //reader may need to be added in the function
-        String lol = reader.readLine();
-
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            while (!accepted) {
+                System.out.println("Enter: (hit/wave)");
+                String input = reader.readLine().toLowerCase().trim();
+                if (input.equals("hit")) {
+                    System.out.println("User hits");
+                    accepted = true;
+                } else if (input.equals("wave")) {
+                    System.out.println("User waves");
+                    accepted = true;
+                } else {
+                    System.out.println("The user did not enter the as instructed");
+                }
+            }
+        }
     }
 }
+
+
